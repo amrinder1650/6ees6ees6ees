364a093592b73d4a42677104c634593ebc6a6001
Amrinder Singh <amrinder1650@gmail.com>
2021-09-20 22:52:35 -0700

    Complete Bare Minimum Requirements

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
+
+  constructor() {
+    super();
+    // this.storage = {};
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+
+
 };
